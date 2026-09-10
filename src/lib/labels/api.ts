89db6ada75116d8e335d2022d3315602823943b3
Yash Chapaneri <yashtatsme@ymail.com>
2026-09-10 22:43:58 +0200

    fix(auth): labels api barrel re-exports

--- src/lib/labels/api.ts
+++ src/lib/labels/api.ts
@@ -1,1 +1,18 @@
-PLACEHOLDER_READ_FROM_DISK
+export type { FashionLabelPage, FashionCollectionPage, AdminHouse } from "./labels-shared";
+export { ensureFashionLabels } from "./labels-shared";
+export {
+  listFashionLabels,
+  listFashionCollections,
+  getFashionLabel,
+  getFashionCollection,
+  listRankedLabels,
+} from "./labels-public";
+export { setLabelScouted, listAdminHouses, setHouseStatus } from "./labels-admin";
+export {
+  getMyHouse,
+  applyHouse,
+  updateMyHouse,
+  listMyCollections,
+  saveMyCollection,
+  deleteMyCollection,
+} from "./labels-owner";
